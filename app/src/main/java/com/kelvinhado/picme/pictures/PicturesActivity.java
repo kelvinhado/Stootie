a0a2e24d8826d5ec365a1c9a417fa1ed911060f0
kelvinhado <kelvinhado@outlook.com>
2017-10-11 22:05:28 +0200

    reuse fragment on orientation change

--- app/src/main/java/com/kelvinhado/picme/pictures/PicturesActivity.java
+++ app/src/main/java/com/kelvinhado/picme/pictures/PicturesActivity.java
@@ -9,6 +9,9 @@ import com.kelvinhado.picme.utils.ActivityUtils;
 
 public class PicturesActivity extends AppCompatActivity {
 
+
+    private PicturesFragment mFragment;
+
     private PicturesPresenter mPresenter;
 
     @Override
@@ -16,21 +19,28 @@ public class PicturesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pictures);
 
-        PicturesFragment picturesFragment =
-                (PicturesFragment) getSupportFragmentManager().findFragmentById(R.id.content_frame);
-        if (picturesFragment == null) {
+        // use saved fragment instance if available
+        if(savedInstanceState != null) {
+            mFragment = (PicturesFragment) getSupportFragmentManager().getFragment(savedInstanceState, "savedFragment");
+        } else {
+            mFragment = (PicturesFragment) getSupportFragmentManager().findFragmentById(R.id.content_frame);
+        }
+
+        if (mFragment == null) {
             // Create the fragment
-            picturesFragment = PicturesFragment.newInstance();
-            ActivityUtils.addFragmentToActivity(
-                    getSupportFragmentManager(), picturesFragment, R.id.content_frame);
+            mFragment = PicturesFragment.newInstance();
+            ActivityUtils.addFragmentToActivity(getSupportFragmentManager(), mFragment, R.id.content_frame);
         }
 
         // add presenter
-        mPresenter = new PicturesPresenter(Injection.providePicturesRepository(this), picturesFragment);
+        mPresenter = new PicturesPresenter(Injection.providePicturesRepository(this), mFragment);
     }
+
+
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
+        getSupportFragmentManager().putFragment(outState, "savedFragment", mFragment);
     }
 }
